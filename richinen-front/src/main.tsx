2e2f0bf2749c7fae6070037b3c33c4e95661048d
Chelsea Sachieko <arashikabane@gmail.com>
2024-08-19 03:35:49 -0600

    Set up react router and routes

--- richinen-front/src/main.tsx
+++ richinen-front/src/main.tsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.scss";
+import Index from "./routes/Index.tsx";
+import Menu from "./routes/Menu.tsx";
+import Contact from "./routes/Contact.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: (
+      <div>
+        <p>Oh kuso an error happened whoop.</p>
+      </div>
+    ),
+    children: [
+      { index: true, element: <Index></Index>},
+      {
+        path: "/menu",
+        element: <Menu />,
+        errorElement: (
+          <div>
+            <p>Oh a smaller error happened, gomen.</p>
+          </div>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+        errorElement: (
+          <div>
+            <p>Oh a smaller error happened, gomen.</p>
+          </div>
+        ),
+      },
+
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
